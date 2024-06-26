3694cd856494cc83d4e1f546750a2ea59adeacca
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2024-06-26 11:09:58 -0300

    how use variable

---
+++ start/variable.js
@@ -0,0 +1,7 @@
+//To declare a variable you need to use let or const in front and assign a value.
+
+const name = "Marcos Júnior"
+
+let age = 19
+
+//the const variable its value cannot be changed, the let can be updated as needed; both forms can be manipulated.
