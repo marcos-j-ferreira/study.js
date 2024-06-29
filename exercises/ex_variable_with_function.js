af437948f7cf2b942b9365b70e0ebca159129867
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2024-06-28 22:03:45 -0300

    functions inside variables

--- exercises/ex_variable_with_function.js
+++ exercises/ex_variable_with_function.js
@@ -1,3 +1,4 @@
+
 let calculator = function(operation, num1, num2){
     switch(operation){
         case "+":
@@ -17,3 +18,16 @@ let calculator = function(operation, num1, num2){
     }
 }
 console.log(calculator("*",10,20))
+
+
+//check if a number is even in the shortest way possible
+
+let aa = (number) => number %2 ==0;
+
+
+// second way
+let numberEven = function(number) {
+    return number % 2 == 0
+  }
+console.log(numberEven(10))
+  
