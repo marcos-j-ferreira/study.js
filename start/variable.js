4aaad102addf584a1e821b7cbbf50314cfee406a
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2024-06-28 21:35:15 -0300

    Function in a variable

--- start/variable.js
+++ start/variable.js
@@ -5,3 +5,21 @@ const name = "Marcos Júnior"
 let age = 19
 
 //the const variable its value cannot be changed, the let can be updated as needed; both forms can be manipulated.
+
+
+// also can use function in a variable
+//anonymous functions 
+
+let even = function(number) {
+    number % 2 == 0 ? console.log("par = "+number) : console.log("Impar = "+ number)
+}
+
+for(let i = 10; i > 0; i--){
+    even(i)
+}
+
+/// has a well-compressed syntax
+
+let agge = (a) => a >= 18?console.log("Maior"):console.log("menor")
+
+age(17)
