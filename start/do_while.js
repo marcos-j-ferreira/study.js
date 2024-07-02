16d73f46f93d03adc10888d9c6faa381d6648ef7
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2024-07-02 20:43:33 -0300

    Do_while in js

---
+++ start/do_while.js
@@ -0,0 +1,9 @@
+// Código é executado antes de verificar a condição, caso a condição seja verdadeira o código se repete.
+
+let number = 10
+
+let condition = 0
+do {
+  condition = +prompt("digite um número")
+} while (condition <= number)
+
